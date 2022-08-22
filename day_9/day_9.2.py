69c4c7bc47b1038098cc8a7df9b3bc52d9b129ae
deerbeck <johannes.hi98@gmx.de>
2022-08-22 19:58:11 +0200

    finished day 9 part 1 and started part 2

--- day_9/day_9.2.py
+++ day_9/day_9.2.py
@@ -2,12 +2,12 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 with open(dir_path + '\input_day_9.txt', "r") as inputfile:
-    workingdata = inputfile.read()
-#     workingdata = """2199943210
-# 3987894921
-# 9856789892
-# 8767896789
-# 9899965678"""
+    #workingdata = inputfile.read()
+    workingdata = """2199943210
+3987894921
+9856789892
+8767896789
+9899965678"""
 
     buf_list = workingdata.split("\n")
     worklist = []
